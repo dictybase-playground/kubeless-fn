6663c26d3b535e8b6ca89847e65fbafdd538aa21
wildlifehexagon <crossthebreeze@gmail.com>
2018-09-21 17:52:08 -0500

    refactor: convert runner into a real helper function

--- geneids/handler.js
+++ geneids/handler.js
@@ -4,7 +4,6 @@ const Minio = require("minio")
 const filepath = require("path")
 const tmp = require("tmp")
 const bunyan = require("bunyan")
-const metadata = require("./metadata.json")
 
 /**
  * Create Bunyan logger
@@ -17,14 +16,16 @@ const logger = bunyan.createLogger({
 /**
  * Instantiate Redis client from env variable
  */
-const client = redis.createClient(
-  `${process.env.REDIS_MASTER_SERVICE_HOST}:${process.env.REDIS_MASTER_SERVICE_PORT}`,
+const redisClient = redis.createClient(
+  `${process.env.REDIS_MASTER_SERVICE_HOST}:${
+    process.env.REDIS_MASTER_SERVICE_PORT
+  }`,
 )
-client.on("connect", () => {
+redisClient.on("connect", () => {
   logger.info("Redis client connected")
 })
-client.on("error", err => {
-  logger.error(`Something went wrong ${err}`)
+redisClient.on("error", err => {
+  logger.error(`Something went wrong starting the Redis client: ${err}`)
 })
 
 /**
@@ -40,11 +41,39 @@ const minioClient = new Minio.Client({
 })
 
 /**
+ * OK response helper
+ */
+const successMessage = (id, name) => {
+  return {
+    type: "gene",
+    id,
+    attributes: {
+      geneName: name,
+      geneId: id,
+    },
+  }
+}
+
+/**
+ * Error message helper
+ */
+const errMessage = (code, msg, url) => {
+  return {
+    status: code,
+    title: msg,
+    detail: msg,
+    source: { pointer: url },
+    meta: { creator: "kubeless function api" },
+  }
+}
+
+/**
  * Put our file handling logic in a class
  */
 class FileHandler {
-  constructor({ fileLocation }) {
+  constructor({ fileLocation, redisClient }) {
     this.fileLocation = fileLocation
+    this.redisClient = redisClient
   }
 
   /**
@@ -81,29 +110,76 @@ class FileHandler {
       })
       // set ID and name as key-value pairs in Redis
       .forEach(item => {
-        client.hset("GENE2NAME/geneids", item[0], item[1], redis.print)
+        this.redisClient.hset("GENE2NAME/geneids", item[0], item[1])
       })
   }
 }
 
-const runner = () => {
-  // create temp folder to store file
-  const tmpObj = tmp.dirSync({ prefix: "minio-" })
-  const folder = tmpObj.name
-  const fileLocation = filepath.join(folder, metadata.file)
+const geneids = event => {
+  const req = event.extensions.request
+  const res = event.extensions.response
+  const path = req.get("x-original-uri")
+  res.set("X-Powered-By", "kubeless")
+  res.set("Content-Type", "application/vnd.api+json")
 
-  // get object from Minio
-  minioClient.fGetObject(metadata.bucket, metadata.file, fileLocation, err => {
-    if (err) {
-      return logger.error(err)
+  // if there is a metadata file, act as POST route
+  if (event.data.bucket) {
+    try {
+      // create temp folder to store file
+      const tmpObj = tmp.dirSync({ prefix: "minio-" })
+      const folder = tmpObj.name
+      const fileLocation = filepath.join(folder, event.data.file)
+
+      // get object from Minio
+      minioClient.fGetObject(
+        event.data.bucket,
+        event.data.file,
+        fileLocation,
+        err => {
+          if (err) {
+            return logger.error("Error getting object: ", error)
+          }
+          logger.info("Object download success!")
+
+          // got the file, now pass the file location into our class
+          const storeFileContent = new FileHandler({
+            fileLocation,
+            redisClient,
+          })
+          // call the cache method
+          storeFileContent.cache()
+        },
+      )
+    } catch (error) {
+      return errMessage(500, error.message, path)
     }
-    logger.info("Object download success!")
+  }
 
-    // got the file, now pass the file location into our class
-    const storeFileContent = new FileHandler({ fileLocation })
-    // call the cache method
-    storeFileContent.cache()
-  })
+  // else get data
+  try {
+    if (redisClient.hexists("GENE2NAME/geneids", req.path)) {
+      return redisClient.hget(req.path, (err, result) => {
+        if (err) {
+          throw err
+        }
+        successMessage(req.path, result)
+      })
+    }
+    return errMessage(404, "no match for route", path)
+  } catch (error) {
+    return errMessage(500, error.message, path)
+  }
 }
 
-runner()
+module.exports = { geneids }
+
+/**
+ * Where I'm at:
+ * 1. I need to test the actual deployment. Is this the right approach inside the geneids function?
+ * 2. The last commit was working locally, with this reading metadata.json, grabbing the specified file from Minio, then caching it in Redis.
+ * 3. I updated this to grab the info from metadata.json as CLI argument. Need to verify that this is in fact correct.
+ * 4. Trying to handle both POST/GET inside geneids function. Initial thought was a conditional to check if the deployment includes a file.
+ * 5. Also, where is the connection between the route and the data?
+ *
+ * Look at previous commit for working local version.
+ */
