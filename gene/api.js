e29f0824fa8f56f5c266fc17dde7306de1a45f88
wildlifehexagon <crossthebreeze@gmail.com>
2018-10-02 10:25:49 -0500

    feat: add function to convert go ID to name

--- gene/api.js
+++ gene/api.js
@@ -64,6 +64,79 @@ const gene2name = async id => {
   }
 }
 
+const makeGoURL = id => {
+  return `https://www.ebi.ac.uk/QuickGO/services/ontology/go/terms/${id}`
+}
+
+const goName2Id = async id => {
+  const hash = "GO2NAME/goids"
+  try {
+    const res = await fetch(makeGoURL(id))
+    const json = await res.json()
+
+    if (res.ok) {
+      await redisClient.hset(hash, json.results[0].id, json.results[0].name)
+      console.log(`Successfully set ${json.results[0].id}:${json.results[0].name} in hash ${hash}`)
+      return {
+        data: {
+          type: "goa",
+          id: json.results[0].id,
+          attributes: {
+            goId: json.results[0].id,
+            goName: json.results[0].name,
+          },
+        },
+      }
+    }
+
+    return {
+      status: 404,
+      title: json.messages[0],
+      detail: json.messages[0],
+      meta: { creator: "kubeless function api" },
+    }
+  } catch (error) {
+    return {
+      status: 500,
+      title: error.message,
+      detail: error.message,
+      meta: { creator: "kubeless function api" },
+    }
+  }
+}
+
+const go2name = async id => {
+  const hash = "GO2NAME/goids"
+
+  try {
+    const exists = await redisClient.hexists(hash, id)
+
+    if (exists === 1) {
+      const value = await redisClient.hget(hash, id)
+      console.log(`successfully found goId ${id} and goName ${value}`)
+      return {
+        data: {
+          type: "goa",
+          id,
+          attributes: {
+            goName: value,
+            goId: id,
+          },
+        },
+      }
+    }
+
+    return goName2Id(id)
+  } catch (error) {
+    return {
+      status: 500,
+      title: error.message,
+      detail: error.message,
+      meta: { creator: "kubeless function api" },
+    }
+  }
+}
+
 // const convertExtensions = ext => {
 //   if (ext === null) {
 //     return null
@@ -225,9 +298,24 @@ const uniprot2Goa = async ids => {
                 }
                 extArr.push(response)
               }
+              if (k.db === "GO") {
+                // eslint-disable-next-line
+                console.log("id: ", k.id)
+                const name = await go2name(`${k.db}:${k.id}`)
+                console.log("name: ", name)
+                const response = {
+                  db: k.db,
+                  id: k.id,
+                  relation: k.relation,
+                  name: name.data.attributes.goName,
+                }
+                extArr.push(response)
+              }
             }
           }
+          // remove old extensions data
           i.attributes.extensions.pop()
+          // replace with new data array
           i.attributes.extensions = extArr
           freshArr.push(i)
         } else if (i.attributes.extensions === null) {
